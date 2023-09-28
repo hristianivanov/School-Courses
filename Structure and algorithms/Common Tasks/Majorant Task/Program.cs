33e85e483333c831d4c52c2d29dda08c18c665af
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-09-28 08:02:51 +0300

    Solved Majorant Task

--- Structure and algorithms/Common Tasks/Majorant Task/Program.cs
+++ Structure and algorithms/Common Tasks/Majorant Task/Program.cs
@@ -4,7 +4,26 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			int[] nums = new int[] { 2, 2, 3, 3, 2, 3, 4, 3, 3 };
+			int count = nums.Length;
+
+			Dictionary<int, int> kvp = new Dictionary<int, int>();
+			int majorant = 0;
+			foreach (int num in nums)
+			{
+				if (kvp.ContainsKey(num))
+					kvp[num]++;
+				else 
+					kvp.Add(num, 1);
+
+				if (kvp[num] > count / 2)
+					majorant = num;
+			}
+
+			if (majorant != 0)
+				Console.WriteLine($"Yes -> {majorant}");
+			else
+				Console.WriteLine("No");
 		}
 	}
 }
