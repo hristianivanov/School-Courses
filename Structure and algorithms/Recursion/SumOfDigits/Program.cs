f20aaed443c9edf39e0ea4d3a0fc4ccedaea584a
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-10-04 11:23:28 +0300

    Solved problem : multiply digits in one number

--- Structure and algorithms/Recursion/SumOfDigits/Program.cs
+++ Structure and algorithms/Recursion/SumOfDigits/Program.cs
@@ -4,8 +4,25 @@
 	{
 		static void Main(string[] args)
 		{
-			
+            Console.WriteLine(MultiplyOfDigits(23));
+        }
+
+		// *
+		private static int MultiplyOfDigits(int n)
+		{
+			if (n < 10)
+			{
+				return n;
+			}
+
+			int lastDigit = n % 10;
+			int remainingDigits = n / 10;
+			int sum = lastDigit * MultiplyOfDigits(remainingDigits);
+
+			return sum;
 		}
+
+		// +
 		private static int SumOfDigits(int n)
 		{
 			if (n < 10)
