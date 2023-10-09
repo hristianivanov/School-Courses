aab64cf58d9308f9e443d858b822521804415cd1
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-10-09 08:17:31 +0300

    solved whether word is palindrome problem

---
+++ Structure and algorithms/Recursion/IsWordPalindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace IsWordPalindrome
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine(IsPalindrome("abcba"));
+        }
+
+		private static bool IsPalindrome(string word)
+		{
+			if (word.Length <= 1)
+				return true;
+
+			if (word[0] != word[word.Length - 1])
+				return false;
+
+			return IsPalindrome(word.Substring(1, word.Length - 2));
+		}
+	}
+}
---
+++ Structure and algorithms/Recursion/Reverse array/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Reverse_array
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums =
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, };
+
+			ReverseArray(nums, 0, nums.Length - 1);
+
+			Console.WriteLine(string.Join(" ", nums));
+		}
+
+		private static int[] ReverseArray(int[] nums, int leftIndex, int rightIndex)
+		{
+			int temp;
+
+			if (leftIndex <= rightIndex)
+			{
+				temp = nums[leftIndex];
+				nums[leftIndex] = nums[rightIndex];
+				nums[rightIndex] = temp;
+
+				ReverseArray(nums, leftIndex + 1, rightIndex - 1);
+			}
+
+			return nums;
+		}
+	}
+}
