4e0391bd770bb8f876e2eec168cd6f0db0455ce6
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-10-12 19:35:53 +0300

    solving problems

---
+++ Structure and algorithms/Recursion/0.Demo/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace _0.Demo
+{
+	internal class Program
+	{
+		static void Merge(int[] arr, int left, int mid, int right)
+		{
+			int n1 = mid - left + 1; // Include mid in the left subarray
+			int n2 = right - mid;
+
+			int[] L = new int[n1];
+			int[] R = new int[n2];
+			int i, j;
+
+			// Copy data to temporary arrays L[] and R[]
+			for (i = 0; i < n1; i++)
+				L[i] = arr[left + i];
+			for (j = 0; j < n2; j++)
+				R[j] = arr[mid + 1 + j];
+
+			i = 0; // Initial index of the first subarray
+			j = 0; // Initial index of the second subarray
+
+			int k = left; // Initial index of the merged subarray
+
+			while (i < n1 && j < n2)
+			{
+				if (L[i] <= R[j])
+				{
+					arr[k] = L[i];
+					i++;
+				}
+				else
+				{
+					arr[k] = R[j];
+					j++;
+				}
+				k++;
+			}
+
+			// Copy the remaining elements of L[], if there are any
+			while (i < n1)
+			{
+				arr[k] = L[i];
+				i++; k++;
+			}
+
+			// Copy the remaining elements of R[], if there are any
+			while (j < n2)
+			{
+				arr[k] = R[j];
+				j++; k++;
+			}
+		}
+
+		static void MergeSort(int[] arr, int left, int right)
+		{
+			if (left < right)
+			{
+				int mid = (left + right) / 2;
+
+				// Sort first and second halves
+				MergeSort(arr, left, mid);
+				MergeSort(arr, mid + 1, right);
+
+				// Merge the sorted halves
+				Merge(arr, left, mid, right);
+			}
+		}
+
+
+		static void Main(string[] args)
+		{
+			int[] nums = new int[]
+			{
+				5,3,2,5,4,347,3,76,534,37,3,2,3,0,-35
+			};
+
+			MergeSort(nums, 0, nums.Length - 1);
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				Console.Write(nums[i] + ", ");
+			}
+		}
+	}
+}
--- Structure and algorithms/Recursion/Merge sort/Program.cs
+++ Structure and algorithms/Recursion/Merge sort/Program.cs
@@ -2,9 +2,64 @@
 {
 	internal class Program
 	{
+		public static void MergeSort(int[] array, int left, int right)
+		{
+			if (left < right)
+			{
+				int middle = left + (right - left) / 2;
+
+				MergeSort(array, left, middle);
+				MergeSort(array, middle + 1, right);
+
+				MergeArray(array, left, middle, right);
+			}
+		}
+
+		public static void MergeArray(int[] array, int left, int middle, int right)
+		{
+			var leftArrayLength = middle - left + 1;
+			var rightArrayLength = right - middle;
+			var leftTempArray = new int[leftArrayLength];
+			var rightTempArray = new int[rightArrayLength];
+			int i, j;
+
+			for (i = 0; i < leftArrayLength; ++i)
+				leftTempArray[i] = array[left + i];
+			for (j = 0; j < rightArrayLength; ++j)
+				rightTempArray[j] = array[middle + 1 + j];
+
+			i = 0; j = 0;
+			int k = left;
+
+			while (i < leftArrayLength && j < rightArrayLength)
+			{
+				if (leftTempArray[i] <= rightTempArray[j])
+					array[k++] = leftTempArray[i++];
+				else
+					array[k++] = rightTempArray[j++];
+			}
+
+			while (i < leftArrayLength)
+				array[k++] = leftTempArray[i++];
+
+			while (j < rightArrayLength)
+				array[k++] = rightTempArray[j++];
+		}
+
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			int[] nums = new int[]
+			{
+				5,3,2,-3,53,-532,3632
+			};
+
+			MergeSort(nums, 0, nums.Length - 1);
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				Console.Write(nums[i] + ", ");
+			}
 		}
 	}
 }
