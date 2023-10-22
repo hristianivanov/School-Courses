e9be48b5311051e50118320b355e56ff87c73e63
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-10-22 20:04:59 +0300

    Solved problem :+1:  for chess queen 8x8

--- Structure and algorithms/Backtracking/Maze matrix/Program.cs
+++ Structure and algorithms/Backtracking/Maze matrix/Program.cs
@@ -2,9 +2,39 @@
 {
 	internal class Program
 	{
+
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			char[,] lab =
+			{
+				{ ' ', ' ', ' ', '*', ' ', ' ', ' ' },
+				{ '*', '*', ' ', '*', ' ', '*', ' ' },
+				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', '*', '*', '*', '*', '*', ' ' },
+				{ ' ', ' ', ' ', ' ', ' ', ' ', 'e' }
+			};
 		}
+
+
+		static void FindPath(char[,] arr, int row,int col)
+		{
+			if (!IsVailid(row,col,arr))
+			{
+				return;
+			}
+
+			if (arr[row,col] == 'e')
+			{
+				Console.WriteLine("Found path");
+				return;
+			}
+
+
+		}
+
+		private static bool IsVailid(int row, int col, char[,] arr)
+			=> row < arr.GetLength(0) && row >= 0
+		&& col < arr.GetLength(1) && col >= 0;
 	}
 }
---
+++ Structure and algorithms/Backtracking/Queen chess/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Queen_chess
+{
+	internal class Program
+	{
+		static int size = 8;
+
+		static void SolveQueensProblem()
+		{
+			int[] queens = new int[size];
+			PlaceQueens(queens, 0);
+		}
+
+		static void PlaceQueens(int[] queens, int row)
+		{
+			if (row == size)
+			{
+				PrintQueens(queens);
+				return;
+			}
+
+			for (int col = 0; col < size; col++)
+			{
+				if (IsSafe(queens, row, col))
+				{
+					queens[row] = col;
+					PlaceQueens(queens, row + 1);
+				}
+			}
+		}
+
+		static bool IsSafe(int[] queens, int row, int col)
+		{
+			for (int i = 0; i < row; i++)
+			{
+				if (queens[i] == col ||
+					queens[i] - i == col - row ||
+					queens[i] + i == col + row)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		static void PrintQueens(int[] queens)
+		{
+			Console.WriteLine("One possible arrangement:");
+			for (int row = 0; row < size; row++)
+			{
+				for (int col = 0; col < size; col++)
+				{
+					if (queens[row] == col)
+						Console.Write("Q ");
+					else
+						Console.Write(". ");
+				}
+
+				Console.WriteLine();
+			}
+
+			Console.WriteLine();
+		}
+		static void Main(string[] args)
+		{
+			SolveQueensProblem();
+		}
+	}
+}
