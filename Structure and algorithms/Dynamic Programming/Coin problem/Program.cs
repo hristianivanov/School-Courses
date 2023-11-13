5a39f5cb5ce194cbefd0053163a2af6f070e9015
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-11-13 08:52:03 +0200

    Refacture code

--- Structure and algorithms/Dynamic Programming/Coin problem/Program.cs
+++ Structure and algorithms/Dynamic Programming/Coin problem/Program.cs
@@ -10,6 +10,7 @@
 
 			int[,] matrix = new int[coins.Length + 1, sum + 1];
 
+
 			for (int j = 1; j < matrix.GetLength(1); j++)
 				matrix[0, j] = 0;
 
@@ -26,14 +27,22 @@
 				}
 			}
 
-			Console.WriteLine(matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1]);
+			int result = matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
 
+			Console.WriteLine(result);
+
+			PrintMatrix(matrix);
+		}
+
+		private static void PrintMatrix(int[,] matrix)
+		{
 			for (int i = 0; i < matrix.GetLength(0); i++)
 			{
 				for (int j = 0; j < matrix.GetLength(1); j++)
 				{
 					Console.Write(matrix[i, j] + " ");
 				}
+
 				Console.WriteLine();
 			}
 		}
