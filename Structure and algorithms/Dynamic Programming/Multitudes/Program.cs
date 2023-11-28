d118597cf9faaea73bf5e318a9d099fe6902f514
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-11-28 11:29:22 +0200

    Solve problem: multitudes

--- Structure and algorithms/Dynamic Programming/Multitudes/Program.cs
+++ Structure and algorithms/Dynamic Programming/Multitudes/Program.cs
@@ -2,20 +2,41 @@
 {
 	internal class Program
 	{
+		//Two partition problem with multitudes
 		//sum(a) = sum( u / a)
 		static void Main(string[] args)
 		{
-			var nums = new int[] { 1, 3, 4, 1, 3, 5 };
-
-			var sum = nums.Sum();
-
-			bool isValid = sum % 2 == 0 &&
-						   nums.Any(n => n <= sum); // ? = 
-
-			if (isValid)
+			int[] set = { 1, 3, 4, 13, 5 };
+			int sum = set.Sum();
+			if (sum % 2 == 1 || set.Any(x => x > sum / 2))
 			{
-
+				Console.WriteLine(false);
+				return;
 			}
+
+			bool[,] matrix = new bool[set.Length, sum / 2];
+			matrix[0, 0] = true;
+			for (int i = 1; i < matrix.GetLength(0); i++)
+			{
+				matrix[0, i] = i == set[0];
+				//if(i == set[0])
+				//{
+				//    matrix[0, i] = true;
+				//}
+				//matrix[0, i] = false;
+			}
+			for (int i = 1; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					matrix[i, j] = matrix[i - 1, j];
+					if (j - set[i] >= 0)
+					{
+						matrix[i, j] = matrix[i - 1, j] || matrix[i - 1, j - set[i]];
+					}
+				}
+			}
+			Console.WriteLine(matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1]);
 		}
 	}
 }
